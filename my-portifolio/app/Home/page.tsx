793f60afa25008e16c867695a78324cd9b203748
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-01-10 23:05:56 -0300

    ajustando o header

--- my-portifolio/app/Home/page.tsx
+++ my-portifolio/app/Home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Intro from "../components/intro";
 import { useState } from "react";
+import { Globe, Moon, Sun  } from "lucide-react";
 
 export default function Home() {
    const [showIntro, setShowIntro] = useState(true);
@@ -14,7 +15,25 @@ export default function Home() {
           <Intro onFinish={handleIntroFinish} />
         </div>)}
 
-        
+    <div className="flex justify-evenly pt-5 items-center gap-100">
+        <img
+            src="/assets/Logo_a.png"
+            alt="Logo"
+            className="w-[54px] h-[41px]"
+          />
+
+          <div className="flex justify-between bg-[#0F0F0F] text-white font-bold rounded-4xl p-3 px-8 gap-20 items-center">
+            <a href=""> SOBRE MIM</a>
+            <a href=""> PROJETOS</a>
+            <a href="">CONTATO</a>
+          </div>
+
+        <div className="flex justify-center gap-10 items-center">
+            <Globe className="w-6 h-6" />
+            <Sun className="w-6 h-6" />
+        </div>
+           
+    </div>
       
     </main>
   );
