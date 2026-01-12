3590e2b97ea787c65e94c0d46b6f9debe2dc73f2
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-01-11 21:32:43 -0300

    Update page.tsx

--- my-portifolio/app/Home/page.tsx
+++ my-portifolio/app/Home/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Intro from "../components/intro";
 import StarBackground from "../components/starbackground";
+import Lua from "../components/lua";
 import { useState } from "react";
 import { Globe, Sun } from "lucide-react";
 
@@ -19,7 +20,7 @@ export default function Home() {
 
       {/* NAVBAR */}
       <header className="w-full flex justify-center pt-6">
-        <div className="w-full max-w-7xl flex items-center justify-between px-8" >
+        <div className="w-full max-w-7xl flex items-center justify-evenly gap-10 px-8" >
           
           {/* Logo */}
           <a href="#">
@@ -51,24 +52,32 @@ export default function Home() {
         </div>
 
       </header>
-        <div className=" flex flex-col justify-center font-bold text-center mt-40">
-            <h1 className="text-3xl text-center">Olá, me chamo Leonardo Ataídes</h1>
-            <div className="flex justify-center mt-5 text-xl">
-                <p className="text-justify w-210">
-                    Sou Desenvolvedor Front-End e Design, crio sites e landing pages que unem beleza, usabilidade e performance, transformo ideias em experiências digitais reais, destacando o que sua empresa ou marca tem de melhor para oferecer.
-                </p>
-            </div>
-            
-            <div className="flex justify-center gap-15 mt-10 items-center">
-            <a href="/cv-leonardo-ataides.pdf" download className="bg-[#0F0F0F] px-6 py-3 rounded-full transition duration-300 ease-out hover:scale-105 active:scale-95">
-                BAIXAR CV
-            </a>
-            <a href="https://wa.me/5561993992964" className="bg-[#0F0F0F] px-6 py-3 rounded-full transition duration-300 ease-out hover:scale-105 active:scale-95">
-                FALE COMIGO
-            </a>                    
-            </div>
 
-        </div>      
+      < Lua />
+
+      {/* Apresentação */}
+      <div className=" relative z-10 flex flex-col justify-center font-bold text-center mt-40">
+        <h1 className="text-3xl text-center">Olá, me chamo Leonardo Ataídes</h1>
+        <div className="flex justify-center mt-5 text-xl">
+            <p className="text-justify w-210">
+                Sou Desenvolvedor Front-End e Design, crio sites e landing pages que unem beleza, usabilidade e performance, transformo ideias em experiências digitais reais, destacando o que sua empresa ou marca tem de melhor para oferecer.
+            </p>
+        </div>
+            
+       <div className="flex justify-center gap-15 mt-10 items-center">
+          <a href="/cv-leonardo-ataides.pdf" download className="bg-[#0F0F0F] px-6 py-3 rounded-full transition duration-300 ease-out hover:scale-105 active:scale-95">
+              BAIXAR CV
+          </a>
+          <a href="https://wa.me/5561993992964" className="bg-[#0F0F0F] px-6 py-3 rounded-full transition duration-300 ease-out hover:scale-105 active:scale-95">
+              FALE COMIGO
+          </a>                    
+        </div>
+
+      </div>
+
+      {/* Projetos */}
+      <h1 className="text-4xl pl-60">PROJETOS</h1>
+      <img src="/assets/CometaSuperior.svg" alt="" className="w-full h-200" />
     </main>
   );
 }
