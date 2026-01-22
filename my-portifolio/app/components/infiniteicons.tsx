a5442450e3ef2ee1501cafabdb0dea1bbfd6c2a0
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-01-21 23:27:33 -0300

    melhorando a animação dos icones

--- my-portifolio/app/components/infiniteicons.tsx
+++ my-portifolio/app/components/infiniteicons.tsx
@@ -1,6 +1,5 @@
-"use client";
+"use client"
 
-import { useEffect, useRef } from "react";
 import {
   SiPython,
   SiFigma,
@@ -12,58 +11,37 @@ import {
   SiGit,
   SiTailwindcss,
   SiJavascript,
-  SiTypescript
+  SiTypescript,
 } from "react-icons/si";
 import { VscodePlain } from "devicons-react";
 import { Database } from "lucide-react";
 
+const icons = [
+  SiPython,
+  SiFigma,
+  SiCss3,
+  SiHtml5,
+  SiNextdotjs,
+  SiGithub,
+  SiReact,
+  SiGit,
+  SiTailwindcss,
+  Database,
+  VscodePlain,
+  SiJavascript,
+  SiTypescript,
+];
+
 export default function InfiniteIcons() {
-  const trackRef = useRef<HTMLDivElement>(null);
-  let x = 0;
-
-  const icons = [
-    SiPython,
-    SiFigma,
-    SiCss3,
-    SiHtml5,
-    SiNextdotjs,
-    SiGithub,
-    SiReact,
-    SiGit,
-    SiTailwindcss,
-    Database,
-    VscodePlain,
-    SiJavascript,
-    SiTypescript,
-  ];
-
-  useEffect(() => {
-    const track = trackRef.current;
-    if (!track) return;
-
-    const speed = 0.4;
-
-    const animate = () => {
-      x -= speed;
-      if (Math.abs(x) >= track.scrollWidth / 2) {
-        x = 0;
-      }
-      track.style.transform = `translateX(${x}px)`;
-      requestAnimationFrame(animate);
-    };
-
-    animate();
-  }, []);
-
   return (
-    <div className="w-full overflow-hidden py-6 mt-14">
-      <div ref={trackRef} className="flex whitespace-nowrap">
-        {[...icons, ...icons, ...icons ].map((Icon, index) => (
+    <div className="relative w-full overflow-hidden py-6 mt-14">
+      <div className="flex animate-marquee">
+        {[...icons, ...icons, ...icons, ...icons].map((Icon, index) => (
           <span
             key={index}
             className="mx-6 inline-flex items-center justify-center text-white"
           >
-            <Icon size={30} color="white" />
+            <Icon size={30} />
           </span>
         ))}
       </div>
