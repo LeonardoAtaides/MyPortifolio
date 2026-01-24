8b2b26f0e0b0958a2342cae50e16a7d3524f933e
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-01-24 17:19:23 -0300

    Update navbar.tsx

--- my-portifolio/app/components/navbar.tsx
+++ my-portifolio/app/components/navbar.tsx
@@ -1,8 +1,11 @@
 "use client"
 
+import { useState } from "react"
 import { Sun, Globe } from "lucide-react"
 
 export default function Navbar() {
+  const [openLang, setOpenLang] = useState(false)
+
   return (
     <header className="w-full flex justify-center pt-6">
       <div className="w-full max-w-7xl flex items-center justify-evenly gap-10 px-8">
@@ -29,11 +32,49 @@ export default function Navbar() {
           </a>
         </nav>
 
-        {/* Ações */}
-        <div className="flex items-center gap-10">
-          <Sun className="w-6 h-6 cursor-pointer hover:opacity-80 transition" />
-          <Globe className="w-6 h-6 cursor-pointer hover:opacity-80 transition" />
-        </div>
+{/* Ações */}
+<div className="relative flex items-center gap-6">
+  <Sun className="w-6 h-6 cursor-pointer hover:opacity-80 transition" />
+
+  {/* Idioma */}
+  <div className="relative">
+    {/* Globe */}
+    <Globe
+      className="w-6 h-6 cursor-pointer hover:opacity-80 transition relative z-10"
+      onClick={() => setOpenLang((prev) => !prev)}
+    />
+
+    {/* Menu flutuante */}
+    <div
+      className={`
+    absolute top-1/2 -translate-y-1/2
+    flex items-center gap-2
+    bg-[#0F0F0F] rounded-full
+    h-6 px-3 -py-1
+    transition-all duration-300 ease-out
+    ${openLang
+      ? "opacity-100 translate-x-0"
+      : "opacity-0 -translate-x-3 pointer-events-none"}
+  `}
+    >
+      {/* Espaço do Globe */}
+      <div className=" -ml-3 w-6 h-6" />
+
+      {/* Idiomas */}
+        <span className="font-bold text-sm cursor-pointer hover:opacity-80 transition leading-none">
+        PT
+        </span>
+        <span className="leading-none">|</span>
+        <span className="font-bold text-sm cursor-pointer hover:opacity-80 transition leading-none">
+        ENG
+        </span>
+    </div>
+  </div>
+</div>
+
+
+
+
 
       </div>
     </header>
