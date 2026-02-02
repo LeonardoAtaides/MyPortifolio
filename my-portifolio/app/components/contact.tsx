300b7c046509e40809780b424557a72f7ad180d4
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-02-01 23:50:57 -0300

    Update contact.tsx

--- my-portifolio/app/components/contact.tsx
+++ my-portifolio/app/components/contact.tsx
@@ -1,89 +1,109 @@
 "use client"
-import{
+
+import {
   SiInstagram,
   SiWhatsapp,
   SiLinkedin,
   SiGmail,
-} from "react-icons/si";
+} from "react-icons/si"
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
 import { useLanguage } from "@/app/context/language"
 import { translations } from "@/lib/translations"
+
 const socialLinks = [
-    {
-        icon:  SiInstagram,
-        href: "https://www.instagram.com/_.ataides._/"
-    },
-    {
-    icon:  SiWhatsapp,
-    href: "https://wa.me/5561998449959"
-    },
-
-    {
-    icon:  SiLinkedin,
-    href: "https://www.linkedin.com/in/leonardo-ataides-a87a04273/"
-    },
-
-     {
-    icon:  SiGmail,
-    href: "mailto:leonardoataidesjesus@gmail.com"
-    }
+  {
+    icon: SiInstagram,
+    href: "https://www.instagram.com/_.ataides._/",
+  },
+  {
+    icon: SiWhatsapp,
+    href: "https://wa.me/5561998449959",
+  },
+  {
+    icon: SiLinkedin,
+    href: "https://www.linkedin.com/in/leonardo-ataides-a87a04273/",
+  },
+  {
+    icon: SiGmail,
+    href: "mailto:leonardoataidesjesus@gmail.com",
+  },
 ]
-
 
 export default function Contact() {
   const { theme } = useTheme()
   const [mounted, setMounted] = useState(false)
-  const { language} = useLanguage()
+  const { language } = useLanguage()
   const t = translations[language]
 
   useEffect(() => {
-  setMounted(true)
+    setMounted(true)
   }, [])
 
   if (!mounted) return null
 
   return (
     <footer className="relative z-20 mt-12">
-     <img src={ theme === "dark" ? "/assets/CometaSuperior.svg" : "/assets/CometaSuperiorVerde.svg"} className="w-full" />
-
-    {theme === "light" && (
+      {/* Cometa */}
       <img
-        src="/assets/BarraVerde.svg"
-        className="w-full absolute "
-        alt="Barra verde"
+        src={
+          theme === "dark"
+            ? "/assets/CometaSuperior.svg"
+            : "/assets/CometaSuperiorVerde.svg"
+        }
+        className="w-full"
+        alt="Cometa"
       />
+
+      {/* Barra verde apenas no light */}
+      {theme === "light" && (
+        <img
+          src="/assets/BarraVerde.svg"
+          className="w-full absolute"
+          alt="Barra verde"
+        />
       )}
 
-      {/* CONTAINER SCROLL */}
-      <div className="flex justify-center " style={{
-        backgroundColor: theme === "dark" ? "#0F0F0F" : "var(--earth)",
-      }}>
-        <div className="flex flex-col justify-center text-centet">
-        <h2 className="text-3xl w-220 text-justify m-6">{t.contact.description}
-        </h2>
+      {/* CONTAINER */}
+      <div
+        className="flex justify-center"
+        style={{
+          backgroundColor: theme === "dark" ? "#0F0F0F" : "var(--earth)",
+        }}
+      >
+        <div className="w-full flex flex-col items-center text-center">
+          {/* Texto */}
+          <h2 className="text-[17px] sm:text-3xl max-w-[90%] sm:max-w-[880px] text-justify m-8 sm:m-0">
+            {t.contact.description}
+          </h2>
 
-        <div className="flex justify-center gap-20 my-12">
-        {socialLinks.map(({ icon: Icon, href }, index) => (
-            <a
-            key={index}
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            <Icon size={70} className="transition duration-300 ease-out
-                        hover:scale-110 " />
-            </a>
-        ))}
+          {/* Ícones */}
+          <div className="flex justify-center gap-8 sm:gap-20 my-5 sm:my-12">
+            {socialLinks.map(({ icon: Icon, href }, index) => (
+              <a
+                key={index}
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon
+                  className="
+                    w-12 h-12
+                    sm:w-[70px] sm:h-[70px]
+                    transition duration-300 ease-out
+                    hover:scale-110
+                  "
+                />
+              </a>
+            ))}
+          </div>
+
+          {/* Copyright */}
+          <p className="text-xs sm:text-base text-center p-4">
+            {t.contact.copyright}
+          </p>
         </div>
-
-         <p className="text-center p-4">{t.contact.copyright}</p> 
-        </div>
-
-        
       </div>
-      
-
     </footer>
   )
 }
