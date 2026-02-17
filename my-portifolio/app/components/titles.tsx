324eef999106b00ed45130db0d3f002a0033dadb
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-02-16 23:36:16 -0300

    animation titles

--- my-portifolio/app/components/titles.tsx
+++ my-portifolio/app/components/titles.tsx
@@ -2,6 +2,7 @@
 
 import { useLanguage } from "@/app/context/language"
 import { translations } from "@/lib/translations"
+import { useEffect, useRef, useState } from "react"
 
 export interface TituloProps {
   tipo: 0 | 1 | 2 | 3
@@ -12,6 +13,32 @@ export default function Titulo({ tipo }: TituloProps) {
   const t = translations[language]
 
   const texto = t.titles[tipo]
+  const ref = useRef<HTMLHeadingElement>(null)
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    const startObserver = () => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setVisible(true)
+            observer.disconnect()
+          }
+        },
+        { threshold: 0.1 }
+      )
+
+      if (ref.current) observer.observe(ref.current)
+
+      return () => observer.disconnect()
+    }
+
+    const timeout = setTimeout(() => {
+      startObserver()
+    }, 3500)
+
+    return () => clearTimeout(timeout)
+  }, [])
 
   if (!texto)
     return <h2 className="text-gray-500">Título não encontrado</h2>
@@ -19,7 +46,18 @@ export default function Titulo({ tipo }: TituloProps) {
   const cor = tipo === 1 || tipo === 2 ? "text-white" : "text-[#fff]"
 
   return (
-    <h1 className={`text-2xl sm:text-4xl mt-5 sm:mt-4 md:mt-5 lg:mt-8 xl:mt-10 pl-8 sm:pl-20 md:pl-24 lg:pl-36 xl:pl-54 2xl:pl-60 absolute uppercase ${cor}`}>
+    <h1
+      ref={ref}
+      className={`
+        text-2xl sm:text-4xl
+        mt-5 sm:mt-4 md:mt-5 lg:mt-8 xl:mt-10
+        pl-8 sm:pl-20 md:pl-24 lg:pl-36 xl:pl-54 2xl:pl-60
+        absolute uppercase
+        transition-all duration-700 ease-out
+        ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-6"}
+        ${cor}
+      `}
+    >
       {texto}
     </h1>
   )
