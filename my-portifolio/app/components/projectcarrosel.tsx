14e22439b9eb59cce27bf11ef2e74ddd6509c18d
Ataídes <162513948+LeonardoAtaides@users.noreply.github.com>
2026-02-22 15:51:10 -0300

    Update projectcarrosel.tsx

--- my-portifolio/app/components/projectcarrosel.tsx
+++ my-portifolio/app/components/projectcarrosel.tsx
@@ -110,7 +110,7 @@ const scrollPrev = () => {
       {theme === "light" && (
         <img
           src="/assets/Grama.svg"
-          className="absolute inset-x-0 top-0 w-[101%] block pointer-events-none"
+          className="absolute top-0 w-full min-w-full block"
           alt="Barra verde"
         />
       )}
